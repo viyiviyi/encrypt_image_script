f459ead7ef1771bf261cf5833acefb74bf6a311a
viyiviyi <39589598+viyiviyi@users.noreply.github.com>
2024-01-03 22:15:54 +0800

    Add files via upload

---
+++ encrypt_image.py
@@ -0,0 +1,191 @@
+from pathlib import Path
+from PIL import PngImagePlugin,_util,ImagePalette
+from PIL import Image as PILImage
+from PIL import Image
+import numpy as np
+import hashlib
+import sys
+
+def get_range(input:str,offset:int,range_len=4):
+    offset = offset % len(input)
+    return (input*2)[offset:offset+range_len]
+
+def get_sha256(input:str):
+    hash_object = hashlib.sha256()
+    hash_object.update(input.encode('utf-8'))
+    return hash_object.hexdigest()
+
+def shuffle_arr(arr,key):
+    sha_key = get_sha256(key)
+    key_len = len(sha_key)
+    arr_len = len(arr)
+    key_offset = 0
+    for i in range(arr_len):
+        to_index = int(get_range(sha_key,key_offset,range_len=8),16) % (arr_len -i)
+        key_offset += 1
+        if key_offset >= key_len: key_offset = 0
+        arr[i],arr[to_index] = arr[to_index],arr[i]
+    return arr
+
+def encrypt_image(image:Image.Image, psw):
+    width = image.width
+    height = image.height
+    x_arr = [i for i in range(width)]
+    shuffle_arr(x_arr,psw)
+    y_arr = [i for i in range(height)]
+    shuffle_arr(y_arr,get_sha256(psw))
+    pixels = image.load()
+    for x in range(width):
+        _x = x_arr[x]
+        for y in range(height):
+            _y = y_arr[y]
+            pixels[x, y], pixels[_x,_y] = pixels[_x,_y],pixels[x, y]
+
+def dencrypt_image(image:Image.Image, psw):
+    width = image.width
+    height = image.height
+    x_arr = [i for i in range(width)]
+    shuffle_arr(x_arr,psw)
+    y_arr = [i for i in range(height)]
+    shuffle_arr(y_arr,get_sha256(psw))
+    pixels = image.load()
+    for x in range(width-1,-1,-1):
+        _x = x_arr[x]
+        for y in range(height-1,-1,-1):
+            _y = y_arr[y]
+            pixels[x, y], pixels[_x,_y] = pixels[_x,_y],pixels[x, y]
+    
+def encrypt_image_v2(image:Image.Image, psw):
+    width = image.width
+    height = image.height
+    x_arr = [i for i in range(width)]
+    shuffle_arr(x_arr,psw)
+    y_arr = [i for i in range(height)]
+    shuffle_arr(y_arr,get_sha256(psw))
+    pixel_array = np.array(image)
+
+    for y in range(height):
+        _y = y_arr[y]
+        temp = pixel_array[y].copy()
+        pixel_array[y] = pixel_array[_y]
+        pixel_array[_y] = temp
+    pixel_array = np.transpose(pixel_array, axes=(1, 0, 2))
+    for x in range(width):
+        _x = x_arr[x]
+        temp = pixel_array[x].copy()
+        pixel_array[x] = pixel_array[_x]
+        pixel_array[_x] = temp
+    pixel_array = np.transpose(pixel_array, axes=(1, 0, 2))
+
+    image.paste(Image.fromarray(pixel_array))
+    return image
+
+def dencrypt_image_v2(image:Image.Image, psw):
+    width = image.width
+    height = image.height
+    x_arr = [i for i in range(width)]
+    shuffle_arr(x_arr,psw)
+    y_arr = [i for i in range(height)]
+    shuffle_arr(y_arr,get_sha256(psw))
+    pixel_array = np.array(image)
+
+    pixel_array = np.transpose(pixel_array, axes=(1, 0, 2))
+    for x in range(width-1,-1,-1):
+        _x = x_arr[x]
+        temp = pixel_array[x].copy()
+        pixel_array[x] = pixel_array[_x]
+        pixel_array[_x] = temp
+    pixel_array = np.transpose(pixel_array, axes=(1, 0, 2))
+    for y in range(height-1,-1,-1):
+        _y = y_arr[y]
+        temp = pixel_array[y].copy()
+        pixel_array[y] = pixel_array[_y]
+        pixel_array[_y] = temp
+
+    image.paste(Image.fromarray(pixel_array))
+    return image
+
+_password = '123qwe'
+
+if PILImage.Image.__name__ != 'EncryptedImage':
+    
+    super_open = PILImage.open
+    
+    class EncryptedImage(PILImage.Image):
+        __name__ = "EncryptedImage"
+        @staticmethod
+        def from_image(image:PILImage.Image):
+            image = image.copy()
+            img = EncryptedImage()
+            img.im = image.im
+            img.mode = image.mode
+            img._size = image.size
+            img.format = image.format
+            if image.mode in ("P", "PA"):
+                if image.palette:
+                    img.palette = image.palette.copy()
+                else:
+                    img.palette = ImagePalette.ImagePalette()
+            img.info = image.info.copy()
+            return img
+            
+        def save(self, fp, format=None, **params):
+            filename = ""
+            if isinstance(fp, Path):
+                filename = str(fp)
+            elif _util.is_path(fp):
+                filename = fp
+            elif fp == sys.stdout:
+                try:
+                    fp = sys.stdout.buffer
+                except AttributeError:
+                    pass
+            if not filename and hasattr(fp, "name") and _util.is_path(fp.name):
+                # only set the name for metadata purposes
+                filename = fp.name
+            
+            if not filename or not _password:
+                # 如果没有密码或不保存到硬盘，直接保存
+                super().save(fp, format = format, **params)
+                return
+            
+            if 'Encrypt' in self.info and (self.info['Encrypt'] == 'pixel_shuffle' or self.info['Encrypt'] == 'pixel_shuffle_2'):
+                super().save(fp, format = format, **params)
+                return
+            
+            encrypt_image_v2(self, get_sha256(_password))
+            self.format = PngImagePlugin.PngImageFile.format
+            if self.info:
+                self.info['Encrypt'] = 'pixel_shuffle_2'
+            pnginfo = params.get('pnginfo', PngImagePlugin.PngInfo())
+            if not pnginfo:
+                pnginfo = PngImagePlugin.PngInfo()
+            pnginfo.add_text('Encrypt', 'pixel_shuffle_2')
+            params.update(pnginfo=pnginfo)
+            super().save(fp, format=self.format, **params)
+            # 保存到文件后解密内存内的图片，让直接在内存内使用时图片正常
+            dencrypt_image_v2(self, get_sha256(_password)) 
+            if self.info:
+                self.info['Encrypt'] = None
+            
+    def open(fp,*args, **kwargs):
+        image = super_open(fp,*args, **kwargs)
+        print(image.info.keys(),image.info.values())
+        if _password and image.format.lower() == PngImagePlugin.PngImageFile.format.lower():
+            pnginfo = image.info or {}
+            if 'Encrypt' in pnginfo and pnginfo["Encrypt"] == 'pixel_shuffle':
+                dencrypt_image(image, get_sha256(_password))
+                pnginfo["Encrypt"] = None
+                image = EncryptedImage.from_image(image=image)
+                return image
+            if 'Encrypt' in pnginfo and pnginfo["Encrypt"] == 'pixel_shuffle_2':
+                dencrypt_image_v2(image, get_sha256(_password))
+                pnginfo["Encrypt"] = None
+                image = EncryptedImage.from_image(image=image)
+                return image
+        return EncryptedImage.from_image(image=image)
+
+    PILImage.Image = EncryptedImage
+    PILImage.open = open
+    
+    print('图片加密插件加载成功')
